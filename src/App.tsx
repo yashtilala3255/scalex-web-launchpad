da388594a1a411cfd4a4ee1fc7401867dfc8b9c5
Yash Tilala <ytilala469@rku.ac.in>
2026-05-25 14:04:08 +0530

    feat: add vercel rewrites and support routing

--- src/App.tsx
+++ src/App.tsx
@@ -77,6 +77,7 @@ const App = () => (
               <Route path="/privacy-policy" element={<PrivacyPolicy />} />
               <Route path="/terms-of-service" element={<TermsOfService />} />
               <Route path="/admin" element={<AdminDashboard />} />
+              <Route path="/Admin" element={<AdminDashboard />} />
               <Route path="*" element={<NotFound />} />
             </Routes>
           </Suspense>
